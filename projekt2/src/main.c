ece2bd17a72657291eb348120bbd84efbe1610b9
stritezsky <stritezskyjirka@seznam.cz>
2025-11-19 11:53:46 +0100

    UART fix

--- projekt2/src/main.c
+++ projekt2/src/main.c
@@ -1,17 +1,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "uart.h"
-#include "si4703.h"
-
+#include <si4703.h>
+#include <twi.h>            // I2C/TWI library for AVR-GCC
+#include <uart.h>    
+#include <avr/interrupt.h>        // Peter Fleury's UART library
 int main(void) {
     uart_init(UART_BAUD_SELECT(9600, F_CPU));
     si4703_power_on();
-
+    //twi_init();
+    //twi_start();
     uint16_t freq = 0;
     uint8_t vol = 0;
     char rds[9];
+    sei();
+    char uart_msg[8];
     uart_puts("FM radio ready\r\n");
-
     while (1) {
         //if (uart_available()) {
             char c = uart_getc();
@@ -41,10 +44,11 @@ int main(void) {
             }
 
             uart_puts("Freq: ");
-            //uart_putc(freq);  //putint
+            itoa(freq, uart_msg, 8);
+            uart_puts(uart_msg);  //putint
             uart_puts(" MHz | Vol: ");
-            //uart_putc(vol);
+            itoa(vol, uart_msg, 16);
+            uart_puts(uart_msg);
             uart_puts("\r\n");
-        //}
+        }
     }
-}
