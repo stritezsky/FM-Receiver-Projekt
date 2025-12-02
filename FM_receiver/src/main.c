71cea3e95971a097220a048c288cfccd9ade1799
kjavurek <krysa.javurek@email.cz>
2025-12-02 16:10:58 +0100

    Update main.c
    add rotary encoder

--- FM_receiver/src/main.c
+++ FM_receiver/src/main.c
@@ -6,24 +6,72 @@
 #include <stdlib.h>
 #include <avr/interrupt.h>        // Peter Fleury's UART library
 #include <oled.h>
+#include <gpio.h> 
+
+
+#define pCLK PD3 //odpovídá pinům 345
+#define pDT PD4
+#define BTN PD5
 
 int main(void) {
     uart_init(UART_BAUD_SELECT(9600, F_CPU));
     twi_init();
     si4703_power_on();
-    uint16_t freq = 0;
-    uint8_t vol = 0;
+    uint16_t freq = 875;  //test zmenil jsem na 875, otestovat
+    uint8_t vol = 9;
     char rds[9];
     sei();
     char uart_msg[8];
     oled_init(OLED_DISP_ON);
-    
+
+    //gpio_mode_output(&DDRB, LED_BUILTIN);
+    uint8_t stateB = gpio_read(&PIND, pCLK); //before
+    uint8_t stateCLK = 0; //CLK
+    uint8_t posENK = 0; //position enkoder
+    //uint8_t stateB = 0; //before
 
     oled_charMode(DOUBLESIZE);
 
     uart_puts("FM radio ready\r\n");
     
     while (1) {
+
+        if (gpio_read(&PIND, BTN) == 0)
+        {
+            _delay_ms(150); // Delay in milliseconds
+            freq = si4703_seek_up();
+        }
+        stateCLK = gpio_read(&PIND, pCLK);
+        if (stateCLK != stateB) {
+            _delay_ms(50); //test
+            // pokud stav pinu DT neodpovídá stavu pinu CLK,
+            // byl pin CLK změněn jako první a rotace byla
+            // po směru hodin, tedy vpravo
+        if (gpio_read(&PIND, pDT) != stateCLK) {
+          _delay_ms(50); //test
+          // vytištění zprávy o směru rotace a přičtení
+          // hodnoty 1 u počítadla pozice enkodéru
+          //Serial.print("Rotace vpravo => | ");
+          //poziceEnkod ++;
+          freq = freq + 1; //mozna + 10?
+          si4703_set_channel(freq);
+        }
+        else {
+            _delay_ms(50); //test
+            // vytištění zprávy o směru rotace a odečtení
+            // hodnoty 1 u počítadla pozice enkodéru
+            //Serial.print("Rotace vlevo  <= | ");
+            //poziceEnkod--;
+            freq = freq - 1; //mozna + 10?
+            si4703_set_channel(freq);
+        }
+        // vytištění aktuální hodnoty pozice enkodéru
+        //Serial.print("Pozice enkoderu: ");
+        //Serial.println(poziceEnkod);
+        }
+        stateB = stateCLK;
+
+
         unsigned int uart_data = uart_getc();
 
         if ((uart_data & 0xFF00) == 0) {
